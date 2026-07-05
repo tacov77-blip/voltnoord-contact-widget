fcc5254a0004b1032ad3dccbe8ccb53f952be2d1
tacov77-blip <tacov77@gmail.com>
2026-07-05 13:48:08 +0200

    Start Volt Widget v2.0

---
+++ docs/volt-widget.js
@@ -0,0 +1,12 @@
+(function () {
+
+    "use strict";
+
+    // Voorkom dubbele installatie
+    if (document.getElementById("volt-widget")) {
+        return;
+    }
+
+    console.log("Volt Widget v2.0 gestart");
+
+})();
